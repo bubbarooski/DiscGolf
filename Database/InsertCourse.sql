30504589e003e51d11df66ff344f3a112f34dbe6
Shane <112229422+bubbarooski@users.noreply.github.com>
2022-11-11 03:45:19 -0500

    Add files via upload

--- Database/InsertCourse.sql
+++ Database/InsertCourse.sql
@@ -1,10 +1,11 @@
 USE DiscGolf
 GO
 
-INSERT INTO Courses (courseID, courseName, courseLayout, coursePar, hole1, hole2, hole3, hole4, hole5, hole6,
-hole7, hole8, hole9, hole10, hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18,
-hole19, hole20, hole21, hole22, hole23, hole24, hole25, hole26, hole27, hole28, hole29, hole30,
-holeA, holeB, holeC, holeD, holeE, holeF)
+INSERT INTO Courses (courseID, courseName, courseLayout, coursePar, hole1, hole1distance, hole2, hole2distance, hole3, hole3distance, hole4, hole4distance, hole5, hole5distance, 
+hole6, hole6distance, hole7, hole7distance, hole8, hole8distance, hole9, hole9distance, hole10, hole10distance, hole11, hole11distance, hole12, hole12distance, hole13, hole13distance,
+hole14, hole14distance, hole15, hole15distance, hole16, hole16distance, hole17, hole17distance, hole18, hole18distance, hole19, hole19distance, hole20, hole20distance, hole21,
+hole21distance, hole22, hole22distance, hole23, hole23distance, hole24, hole24distance, hole25, hole25distance, hole26, hole26distance, hole27, hole27distance, hole28, hole28distance,
+hole29, hole29distance, hole30, hole30distance, holeA, holeAdistance, holeB, holeBdistance, holeC, holeCdistance, holeD, holeDdistance, holeE, holeEdistance, holeF, holeFdistance)
 
 VALUES (
 --courseID (not null)
@@ -16,111 +17,111 @@ VALUES (
 --coursePar (not null)
 '76',
 
---Hole 1
-'3',
+--Hole 1 and distance
+'3', '345',
 
---Hole 2
-'3',
+--Hole 2 and distance
+'3', '337',
 
---Hole 3
-'3',
+--Hole 3 and distance
+'3', '323',
 
---Hole 4
-'3',
+--Hole 4 and distance
+'3', '312',
 
---Hole 5
-'3',
+--Hole 5 and distance
+'3', '330',
 
---Hole 6
-'3',
+--Hole 6 and distance
+'3', '217',
 
---Hole 7
-'3',
+--Hole 7 and distance
+'4', '335',
 
---Hole 8
-'3',
+--Hole 8 and distance
+'3', '346',
 
---Hole 9
-'3',
+--Hole 9 and distance
+'3', '309',
 
---Hole 10
-'3',
+--Hole 10 and distance
+'3', '312',
 
---Hole 11
-'3',
+--Hole 11 and distance
+'3', '280',
 
---Hole 12
-'3',
+--Hole 12 and distance
+'3', '332',
 
---Hole 13
-'4',
+--Hole 13 and distance
+'4', '471',
 
---Hole 14
-'3',
+--Hole 14 and distance
+'3', '252',
 
---Hole 15
-'3',
+--Hole 15 and distance
+'3', '230',
 
---Hole 16
-'3',
+--Hole 16 and distance
+'3', '370',
 
---Hole 17
-'3',
+--Hole 17 and distance
+'3', '333',
 
---Hole 18
-'3',
+--Hole 18 and distance
+'3', '383',
 
---Hole 19
-'',
+--Hole 19 and distance
+'', '',
 
---Hole 20
-'',
+--Hole 20 and distance
+'', '',
 
---Hole 21
-'',
+--Hole 21 and distance
+'', '',
 
---Hole 22
-'',
+--Hole 22 and distance
+'', '',
 
---Hole 23
-'',
+--Hole 23 and distance
+'', '',
 
---Hole 24
-'',
+--Hole 24 and distance
+'', '',
 
---Hole 25
-'',
+--Hole 25 and distance
+'', '',
 
---Hole 26
-'',
+--Hole 26 and distance
+'', '',
 
---Hole 27
-'',
+--Hole 27 and distance
+'', '',
 
---Hole 28
-'',
+--Hole 28 and distance
+'', '',
 
---Hole 29
-'',
+--Hole 29 and distance
+'', '',
 
---Hole 30
-'',
+--Hole 30 and distance
+'', '',
 
---Hole A
-'3',
+--Hole A and distance
+'3', '217',
 
---Hole B
-'4',
+--Hole B and distance
+'4', '664',
 
---Hole C
-'3',
+--Hole C and distance
+'3', '206',
 
---Hole D
-'4',
+--Hole D and distance
+'4', '595',
 
---Hole E
-'3',
+--Hole E and distance
+'3', '242',
 
---Hole F
-'3'
+--Hole F and distance
+'3', '314'
 )
