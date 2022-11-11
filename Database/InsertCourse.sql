45d8d282ebe39b7d7ffaa499dcbc35488a1b0ef3
Shane <112229422+bubbarooski@users.noreply.github.com>
2022-11-11 02:45:07 -0500

    Add files via upload
    An example of inserting a new course

---
+++ Database/InsertCourse.sql
@@ -0,0 +1,126 @@
+USE DiscGolf
+GO
+
+INSERT INTO Courses (courseID, courseName, courseLayout, coursePar, hole1, hole2, hole3, hole4, hole5, hole6,
+hole7, hole8, hole9, hole10, hole11, hole12, hole13, hole14, hole15, hole16, hole17, hole18,
+hole19, hole20, hole21, hole22, hole23, hole24, hole25, hole26, hole27, hole28, hole29, hole30,
+holeA, holeB, holeC, holeD, holeE, holeF)
+
+VALUES (
+--courseID (not null)
+'1',
+--courseName (not null)
+'Fore Palms',
+--courseLayout
+'Main White to Yellow',
+--coursePar (not null)
+'76',
+
+--Hole 1
+'3',
+
+--Hole 2
+'3',
+
+--Hole 3
+'3',
+
+--Hole 4
+'3',
+
+--Hole 5
+'3',
+
+--Hole 6
+'3',
+
+--Hole 7
+'3',
+
+--Hole 8
+'3',
+
+--Hole 9
+'3',
+
+--Hole 10
+'3',
+
+--Hole 11
+'3',
+
+--Hole 12
+'3',
+
+--Hole 13
+'4',
+
+--Hole 14
+'3',
+
+--Hole 15
+'3',
+
+--Hole 16
+'3',
+
+--Hole 17
+'3',
+
+--Hole 18
+'3',
+
+--Hole 19
+'',
+
+--Hole 20
+'',
+
+--Hole 21
+'',
+
+--Hole 22
+'',
+
+--Hole 23
+'',
+
+--Hole 24
+'',
+
+--Hole 25
+'',
+
+--Hole 26
+'',
+
+--Hole 27
+'',
+
+--Hole 28
+'',
+
+--Hole 29
+'',
+
+--Hole 30
+'',
+
+--Hole A
+'3',
+
+--Hole B
+'4',
+
+--Hole C
+'3',
+
+--Hole D
+'4',
+
+--Hole E
+'3',
+
+--Hole F
+'3'
+)
